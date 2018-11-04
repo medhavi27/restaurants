ae498597aee093271128b83927ca2b0f35242b62
Medhavi Gandhi <medhavi@medhavis-macbook-air.local>
2018-11-03 22:24:17 -0400

    Initial Commit

---
+++ mg2252_a5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mg2252_a5
+//
+//  Created by Medhavi Gandhi on 11/3/18.
+//  Copyright © 2018 Medhavi Gandhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
